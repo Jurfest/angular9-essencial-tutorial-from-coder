35de4702fa81fa6e54839a4c420e0c4cb1e1cec3
Jurfest <jurfest.apps@gmail.com>
2020-05-02 19:15:11 -0300

    navigate to product-create by button

--- src/app/views/product-crud/product-crud.component.ts
+++ src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+
 
 @Component({
   selector: 'app-product-crud',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductCrudComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  navigateToProductCreate() {
+    this._router.navigate(['/products/create']);
   }
 
 }
